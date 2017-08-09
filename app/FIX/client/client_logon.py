d90f5ad403f2fef562b3c9a0edb7247bf4665a43
Jonny Martin <jonny.martin@neueda.com>
2017-08-09 15:06:09 +0100

    Added file as default param

--- app/FIX/client/client_logon.py
+++ app/FIX/client/client_logon.py
@@ -4,19 +4,21 @@ import quickfix as fix
 from   qfix_app_BME import Application
 
 
-try:
-    thisFile = sys.argv[1]
-    settings = fix.SessionSettings(thisFile)
-    application = Application()
-    storeFactory = fix.FileStoreFactory(settings)
-    #logFactory = fix.ScreenLogFactory(settings)
-    logFactory = fix.FileLogFactory(settings)
-    initiator = fix.SocketInitiator(application,
-                                       storeFactory, 
-                                       settings,
-                                       logFactory)
-    initiator.start()
-    while 1:
-        time.sleep(1)
-except (fix.ConfigError, fix.RuntimeError) as e:
-    print(e)
+def logon(thisFile):
+
+    try:
+        settings = fix.SessionSettings(thisFile)
+        application = Application()
+        storeFactory = fix.FileStoreFactory(settings)
+        #logFactory = fix.ScreenLogFactory(settings)
+        logFactory = fix.FileLogFactory(settings)
+        initiator = fix.SocketInitiator(application,
+                                           storeFactory,
+                                           settings,
+                                           logFactory)
+        initiator.start()
+        while 1:
+            time.sleep(1)
+    except (fix.ConfigError, fix.RuntimeError) as e:
+        print(e)
+
