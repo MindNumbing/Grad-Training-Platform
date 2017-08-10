d5ee710a259b0de65f7bd8f8651e2538475688a7
ryanswain1 <ryan.swain@neueda.com>
2017-08-10 14:03:50 +0100

    changed connection check in base html

--- app/Flask/views.py
+++ app/Flask/views.py
@@ -5,18 +5,19 @@ from app.Flask import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', connection_status="connected")
+
 
 @app.route('/new_order')
 def new_order():
-    return render_template('new_order.html')
+    return render_template('new_order.html', connection_status="connected")
 
 
 @app.route('/orders')
 def orders():
-    return render_template('orders.html')
+    return render_template('orders.html', connection_status="connected")
 
 
 @app.route('/access')
 def access():
-    return render_template('access.html')
+    return render_template('access.html', connection_status="connected")
