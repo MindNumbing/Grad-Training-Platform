894d0b4b74361877e9ee0a9e184ab12f4487e1e7
DigitalSenseDesign <DigitalSenseDesign@gmail.com>
2017-08-10 13:41:30 +0100

    Created connection wrapper
    Connection wrapper connects fix to BME, disconnects fix from BME and
    tests connection between BME and FIX
    Deleted unneeded config file
    Renamed client logon to fix client

--- app/FIX/client/fix_client.py
+++ app/FIX/client/fix_client.py
@@ -3,24 +3,32 @@ import quickfix
 from qfix_app_BME import Application
 
 isConnected = None
+keep_open = None
 
-
-def logon(this_file):
-
+def connect(config_file):
+    keep_open = True
+    
     try:
-        settings = quickfix.SessionSettings(this_file)
+        settings = quickfix.SessionSettings(config_file)
         application = Application()
         store_factory = quickfix.FileStoreFactory(settings)
-        #logFactory = fix.ScreenLogFactory(settings)
         log_factory = quickfix.FileLogFactory(settings)
         initiator = quickfix.SocketInitiator(application,
                                              store_factory,
                                              settings,
                                              log_factory)
+
         initiator.start()
-        while 1:
+
+        while keep_open:
             time.sleep(1)
             global isConnected
             isConnected = initiator.isLoggedOn()
-    except (fix.ConfigError, fix.RuntimeError) as e:
+
+        initiator.stop()
+    except (quickfix.ConfigError, quickfix.RuntimeError) as e:
         print(e)
+
+def disconnect():
+    global keep_open
+    keep_open = False
