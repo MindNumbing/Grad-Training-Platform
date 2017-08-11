558b079285bf812f589a31532ee76f9601dca424
ryanswain1 <ryan.swain@neueda.com>
2017-08-11 12:40:54 +0100

    minor changes to base, views and css

--- app/Flask/views.py
+++ app/Flask/views.py
@@ -1,12 +1,19 @@
 from flask import render_template
 from app.Flask import app
-from app.FIX.client import fix_connection
+from app.FIX.client import fix_connection_test
 
-connection = fix_connection.isConnected()
+connection = fix_connection_test.isConnected()
+
 
 @app.route('/')
 @app.route('/index')
 def index():
+    return render_template('index.html', connection_status=connection)
+
+
+@app.route('/fix_connection_test', methods=['POST'])
+def test_connection():
+    fix_connection_test.connect_client('FIXT-1.1')
     return render_template('index.html', connection_status=connection)
 
 
