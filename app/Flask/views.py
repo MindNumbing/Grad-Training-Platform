013f9fd2fd7b06fb1ff55b401b6364ad18428cb1
Steven Park <spark1289@hotmail.co.uk>
2017-08-11 14:11:52 +0100

    added comments to the connection function and moved connection function into the index page to show functionality

--- app/Flask/views.py
+++ app/Flask/views.py
@@ -2,31 +2,34 @@ from flask import render_template
 from app.Flask import app
 from app.FIX.client import fix_connection_test
 
-connection = fix_connection_test.isConnected()
-
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', connection_status=connection)
+    test_connection()
+    return render_template('index.html', connection_status=fix_connection_test.isConnected())
 
 
 @app.route('/fix_connection_test', methods=['POST'])
 def test_connection():
+    '''This takes the function from fix_connection_test to send a connection to the BME server
+        Returns:
+            A new index page is requested to show the updated server status
+    '''
     fix_connection_test.connect_client('FIXT-1.1')
-    return render_template('index.html', connection_status=connection)
+    return render_template('index.html')
 
 
 @app.route('/new_order')
 def new_order():
-    return render_template('new_order.html', connection_status=connection)
+    return render_template('new_order.html', connection_status=fix_connection_test.isConnected())
 
 
 @app.route('/orders')
 def orders():
-    return render_template('orders.html', connection_status=connection)
+    return render_template('orders.html', connection_status=fix_connection_test.isConnected())
 
 
 @app.route('/access')
 def access():
-    return render_template('access.html', connection_status=connection)
+    return render_template('access.html', connection_status=fix_connection_test.isConnected())
